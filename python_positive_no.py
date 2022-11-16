724e1412ede5ab8c3a02311473509935b2f748e9
binduvekariya <117823697+binduvekariya@users.noreply.github.com>
2022-11-16 21:29:31 +0530

    Add files via upload

---
+++ python_positive_no.py
@@ -0,0 +1,18 @@
+# print all positive number in range
+
+list1 = []
+n = int(input("enter size of list: "))
+for j in range(0,n):
+    e = int(input())
+    list1.append(e)
+
+# list = [2, 6, -3, 5, -7, 8, -7, 9]
+
+for i in list1:
+    
+    if(i>=0):
+        print(i,end=" ")
+
+    
+
+
